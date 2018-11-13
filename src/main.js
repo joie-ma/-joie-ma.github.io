273f5bc51477fd277c7c6bf1d24cae5f5064ba47
enet <qzhou@enet.com.cn>
2018-11-13 13:39:40 +0800

    我是下午修改的

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+//我修改了 2018.11.13下午
 import Vue from 'vue';
 import App from './App';
 import router from './router';
